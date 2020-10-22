7a762f2401d5e72335079b546bbc526faf1315f5
unknown <jdrosenblatt18@gmail.com>
2020-10-22 18:16:18 -0500

    Fixing app.jsx template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import Routes from './Routes';
-import { Nav, Navbar, Form, Button, Dropdown } from 'react-bootstrap';
-import { Redirect, Link } from 'react-router-dom';
-import { SearchOutlined } from '@ant-design/icons';
-import logo from './logoGreenTransparent.png';
 
 
 class App extends React.Component{
@@ -51,70 +47,10 @@ class App extends React.Component{
       let redirectURL = `/product/${this.state.searchQuery}`;
       this.setState({ searchQuery: "" });
       this.setState({ search: false });
-      return(<Redirect to={redirectURL} />);
+      return(<></>);
     }
 
-    return (
-      <div className="">
-        <Navbar id="nav" bg="light" variant="light" expand="lg" onSelect={this.setNavExpanded} expanded={this.state.navExpanded}>
-					<Navbar.Brand >
-            <Link id="navLink" className="" to="/" onClick={() => this.setNavExpanded(false)}>Foogle</Link>
-            <Link to="/"><img id = "navImg" src = {logo} ></img></Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => this.setNavExpanded(this.state.navExpanded ? false : "expanded")}/>
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="m-auto">
-                {sessionStorage.getItem("auth")
-                  ? <>
-                      <Navbar.Brand>
-                        <Link id = "navLink" to="/" onClick={ e => { this.setNavExpanded(false); this.handleLogout(e) } } >Logout</Link>
-                      </Navbar.Brand>
-                      <Navbar.Brand>
-                        <Link id = "navLink" to={`/${window.sessionStorage.getItem("username")}`} onClick={() => this.setNavExpanded(false)}>Profile</Link>
-                      </Navbar.Brand>
-                      <Navbar.Brand>
-                        <Link id = "navLink" to={`/foodsNew`} onClick={() => this.setNavExpanded(false)}>Foods</Link>
-                      </Navbar.Brand>
-                    </>
-                  : <>
-                      <Navbar.Brand>
-                        <Link id = "navLink" to="/login" onClick={() => this.setNavExpanded(false)}>Login</Link>
-                      </Navbar.Brand>
-                    </>
-                }
-                <Navbar.Brand>
-                  <Link id = "navLink" to = "/about" onClick={() => this.setNavExpanded(false)}>About</Link>
-                </Navbar.Brand>
-              </Nav>
-            
-              <Form inline>
-                <Form.Control type="text"
-                              placeholder="Search for a Food"
-                              className=" mr-sm-2"
-                              value={this.state.searchQuery}
-                              onChange={ e => this.setState({ searchQuery: e.target.value }) }
-                              onKeyPress={ e => this.keyPressed(e) } />
-                <Dropdown as={Button.Group}>
-                  <Button id="buttonRules"
-                          type="button"
-                          onClick={ e => { this.setNavExpanded(false); this.searchResult(e) }}
-                          disabled={ !(this.state.searchQuery.length > 0)}>
-                          <SearchOutlined className="align-middle" />
-                  </Button>
-                  <Dropdown.Toggle split type="button" id="buttonRules" />
-                  <Dropdown.Menu id="dropdown-menu">
-                    <Dropdown.Item href="/search">Advanced Search</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </Form> 
-            </Navbar.Collapse>
-				</Navbar>
-        
-        <div className="container-fluid py-3 content">
-          <Routes className="m-1" appProps={ this.state } />
-        </div>
-      </div>
-    );
+    return <></>;
   }
 
 }
--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,33 +1,5 @@
 import React from 'react';
-import { Switch } from 'react-router-dom';
-import AppliedRoute from './AppliedRoute';
-import { ProfileView, ProfileEditor, LikesView, SavedView, RecipeView, RecipeEditor } from './Profile';
-import { loginLanding } from './Login'
-import { ProductView, ProductEditor } from './Products';
-import { landingPage, about, gettingStarted, backgroundPage, Foods } from './LandingPage';
-import { AdvancedSearch } from './Search'
 
 export default function Routes({ appProps }) {
-    return (
-      <Switch>
-        <AppliedRoute path="/" exact component={landingPage} appProps={appProps} />
-        <AppliedRoute path="/about" exact component={about} appProps={appProps} />
-        <AppliedRoute path="/foodsNew" exact component={Foods} appProps={appProps} />
-        <AppliedRoute path="/gettingStarted" exact component={gettingStarted} appProps={appProps} />
-        <AppliedRoute path="/backgroundPage" exact component={backgroundPage} appProps={appProps} />
-        <AppliedRoute path="/login" exact component={loginLanding} appProps={appProps} />
-        <AppliedRoute path="/search" exact component={AdvancedSearch} appProps={appProps} />
-        <AppliedRoute path="/:username" exact component={ProfileView} appProps={appProps} />
-        <AppliedRoute path="/:username/likes" exact component={LikesView} appProps={appProps} />
-        <AppliedRoute path="/:username/saved" exact component={SavedView} appProps={appProps} />
-        <AppliedRoute path="/:username/edit" exact component={ProfileEditor} appProps={appProps} />
-        <AppliedRoute path="/:username/recipes" exact component={RecipeView} appProps={appProps} />
-        <AppliedRoute path="/:username/recipes/:recipeName" exact component={RecipeEditor} appProps={appProps} />
-        <AppliedRoute path="/product/:name" exact component={ProductView} appProps={appProps} />
-        <AppliedRoute path="/product/:name/edit" exact component={ProductEditor} appProps={appProps} />
-        
-        { /* Add catch for routes not found */ }
-        {/* <Route component={NotFound} /> */}
-      </Switch>
-    );
+    return <></>;
 }
