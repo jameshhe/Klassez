500612132900bde0eedc889d8cf9afec4555237e
unknown <jdrosenblatt18@gmail.com>
2020-11-04 23:02:03 -0600

    Created a basic component for displaying the list of classes

---
+++ frontend/src/Models/class.js
@@ -0,0 +1,11 @@
+export class Class {
+    constructor(id, name, classCode, professor, startTime, endTime, days) {
+        this.id = id
+        this.name = name
+        this.classCode = classCode
+        this.professor = professor
+        this.startTime = startTime
+        this.endTime = endTime
+        this.days = days
+    }
+}
---
+++ frontend/src/components/classList/classList.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import "./classList.css"
+
+export class ClassList extends React.Component{
+    constructor(props){
+        super()
+        this.state = {}
+
+        this.classes = props.classes;
+        this.classes = this.classes.sort((x, y) => (
+            x.classCode > y.classCode ? 1 : -1)
+        )
+        
+        this.previous = "";
+    }
+
+    render(){
+        return <table>
+        <tbody>
+    
+    {
+        this.classes.map((x, i) => <tr key={i}>
+            <td>
+                <table>
+                    <tbody>
+                        {
+                            (this.previous !== x.classCode) ? 
+                            <tr>
+                            <td><h2>{x.classCode} - {x.name}</h2></td>
+                            </tr>
+                            :
+                            <></>
+                        }
+                        <div className="hidden">
+                            {this.previous = x.classCode}
+                        </div>
+                        
+                        <tr>
+                            <td>
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th className="border">ID</th>
+                                            <th className="border">Instructor</th>
+                                            <th className="border">Days and Times</th>
+                                            <th className="border"></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td className="border">{x.id}</td>
+                                            <td className="border">{x.professor}</td>
+                                            <td className="border">{x.days} {x.startTime}-{x.endTime}</td>
+                                            <td className="border">View Details</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+        )
+    }
+        </tbody>
+    </table>;
+    }
+}
