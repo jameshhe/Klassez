5e7dc705c757bcb9dea8f0e9bf7f9fc5094d333c
SamYassien <syassien@smu.edu>
2020-11-12 15:36:57 -0600

    added routes for class list and class by Id

--- backend/server.js
+++ backend/server.js
@@ -15,5 +15,30 @@ app.use(bodyParser.json()) // json parsing
 app.use(express.json()) // enable json parsing???
 app.use(cors()) // cross origin resource sharing
 
+// create router
+var router = express.Router();
+
+// REGISTER  ROUTES
+app.use('/api', router);
+
+// @route   GET api/classes
+// @desc    GET all classes
+router.get('/classes', function (req, res) {
+	con.query("SELECT * FROM classes", function (err, result, fields) {
+		if (err) throw err;
+		res.end(JSON.stringify(result)); // Result in JSON format
+	});
+});
+
+// @route   GET api/classes/:id
+// @desc    GET class info by classID
+router.get('/classes/:id', function (req, res) {
+    var classID 		= req.param.id
+	con.query("SELECT * FROM classes WHERE classID = ?",classID ,function (err, result, fields) {
+		if (err) throw err;
+		res.end(JSON.stringify(result)); // Result in JSON format
+	});
+});
+
 // connect
 app.listen(port, () => console.log(`backend running on http://localhost:${port}`)) // port
