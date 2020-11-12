29f2d69a3ba5fd79e73d1dfdac7a800824be1839
unknown <jdrosenblatt18@gmail.com>
2020-11-12 17:35:48 -0600

    added seats remaining ot the class model

--- frontend/src/Models/class.js
+++ frontend/src/Models/class.js
@@ -1,5 +1,5 @@
 export class Class {
-    constructor(id, name, classCode, professor, startTime, endTime, days) {
+    constructor(id, name, classCode, professor, startTime, endTime, days, seatsRemaining) {
         this.id = id
         this.name = name
         this.classCode = classCode
@@ -7,5 +7,6 @@ export class Class {
         this.startTime = startTime
         this.endTime = endTime
         this.days = days
+        this.seatsRemaining = seatsRemaining
     }
 }
