8b355feda68967a0f44bfdb1af00ea1633021c2a
unknown <jdrosenblatt18@gmail.com>
2020-11-19 17:39:38 -0600

    save

--- frontend/src/components/classForm.jsx
+++ frontend/src/components/classForm.jsx
@@ -119,129 +119,131 @@ class ClassForm extends React.Component {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="card card-signin flex-row my-5">
-                        <div className="card-body">
-                            <h5 className="card-title text-center">Class Form</h5>
-                            <form className="form-signin" onSubmit={this.onSubmit}>
-                                <div className="form-label-group">
-                                    <input
-                                        onChange={this.onChange}
-                                        value={this.state.name}
-                                        id="name"
-                                        type="text"
-                                        className="form-control"
-                                    />
-                                    <label htmlFor="name">Name</label>
-                                </div>
+                    <div className="col-7 col-7 mx-auto">
+                        <div className="card  my-5">
+                            <div className="card-body">
+                                <h5 className="card-title text-center">Class Form</h5>
+                                <form className="form-signin" onSubmit={this.onSubmit}>
+                                    <div className="form-label-group">
+                                        <input
+                                            onChange={this.onChange}
+                                            value={this.state.name}
+                                            id="name"
+                                            type="text"
+                                            className="form-control"
+                                        />
+                                        <label htmlFor="name">Name</label>
+                                    </div>
 
-                                <div className="form-label-group">
-                                    <input
-                                        onChange={this.onChange}
-                                        value={this.state.classCode}
-                                        id="classCode"
-                                        type="text"
-                                        className="form-control"
-                                    />
-                                    <label htmlFor="classCode">Class Code</label>
-                                </div>
+                                    <div className="form-label-group">
+                                        <input
+                                            onChange={this.onChange}
+                                            value={this.state.classCode}
+                                            id="classCode"
+                                            type="text"
+                                            className="form-control"
+                                        />
+                                        <label htmlFor="classCode">Class Code</label>
+                                    </div>
 
-                                <div className="form-label-group">
-                                    <input
-                                        onChange={this.onChange}
-                                        value={this.state.professor}
-                                        id="professor"
-                                        type="text"
-                                        className="form-control"
-                                    />
-                                    <label htmlFor="professor">Professor</label>
-                                </div>
+                                    <div className="form-label-group">
+                                        <input
+                                            onChange={this.onChange}
+                                            value={this.state.professor}
+                                            id="professor"
+                                            type="text"
+                                            className="form-control"
+                                        />
+                                        <label htmlFor="professor">Professor</label>
+                                    </div>
 
-                                <div className="form-label-group">
-                                    <input
-                                        onChange={this.onChange}
-                                        value={this.state.startTime}
-                                        id="startTime"
-                                        type="text"
-                                        className="form-control"
-                                    />
-                                    <label htmlFor="startTime">Start Time</label>
-                                </div>
+                                    <div className="form-label-group">
+                                        <input
+                                            onChange={this.onChange}
+                                            value={this.state.startTime}
+                                            id="startTime"
+                                            type="text"
+                                            className="form-control"
+                                        />
+                                        <label htmlFor="startTime">Start Time</label>
+                                    </div>
 
-                                <div className="form-label-group">
-                                    <input
-                                        onChange={this.onChange}
-                                        value={this.state.endTime}
-                                        id="endTime"
-                                        type="text"
-                                        className="form-control"
-                                    />
-                                    <label htmlFor="endTime">End Time</label>
-                                </div>
+                                    <div className="form-label-group">
+                                        <input
+                                            onChange={this.onChange}
+                                            value={this.state.endTime}
+                                            id="endTime"
+                                            type="text"
+                                            className="form-control"
+                                        />
+                                        <label htmlFor="endTime">End Time</label>
+                                    </div>
 
-                                <div className="form-check form-check-inline inner_padding">
-                                    <input
-                                        onChange={() => this.setState({monday: !this.state.monday})}
-                                        value={this.state.monday}
-                                        id="monday"
-                                        type="checkbox"
-                                        className="form-check-input"
-                                    />
-                                    <label className="form-check-label" htmlFor="monday">Monday</label>
-                                    
-                                    <input
-                                        onChange={() => this.setState({tuesday: !this.state.tuesday})}
-                                        value={this.state.tuesday}
-                                        id="tuesday"
-                                        type="checkbox"
-                                        className="form-check-input"
-                                    />
-                                    <label className="form-check-label" htmlFor="tuesday">Tuesday</label>
-                                    
-                                    <input
-                                        onChange={() => this.setState({wednesday: !this.state.wednesday})}
-                                        value={this.state.wednesday}
-                                        id="wednesday"
-                                        type="checkbox"
-                                        className="form-check-input"
-                                    />
-                                    <label className="form-check-label" htmlFor="wednesday">Wednesday</label>
-                                    
-                                    <input
-                                        onChange={() => this.setState({thursday: !this.state.thursday})}
-                                        value={this.state.thursday}
-                                        id="thursday"
-                                        type="checkbox"
-                                        className="form-check-input"
-                                    />
-                                    <label className="form-check-label" htmlFor="thursday">Thursday</label>
-                                    
-                                    <input
-                                        onChange={() => this.setState({friday: !this.state.friday})}
-                                        value={this.state.friday}
-                                        id="friday"
-                                        type="checkbox"
-                                        className="form-check-input"
-                                    />
-                                    <label className="form-check-label" htmlFor="friday">Friday</label>
-                                </div>
+                                    <div className="form-check form-check-inline inner_padding">
+                                        <input
+                                            onChange={() => this.setState({monday: !this.state.monday})}
+                                            value={this.state.monday}
+                                            id="monday"
+                                            type="checkbox"
+                                            className="form-check-input"
+                                        />
+                                        <label className="form-check-label" htmlFor="monday">Monday</label>
+                                        
+                                        <input
+                                            onChange={() => this.setState({tuesday: !this.state.tuesday})}
+                                            value={this.state.tuesday}
+                                            id="tuesday"
+                                            type="checkbox"
+                                            className="form-check-input"
+                                        />
+                                        <label className="form-check-label" htmlFor="tuesday">Tuesday</label>
+                                        
+                                        <input
+                                            onChange={() => this.setState({wednesday: !this.state.wednesday})}
+                                            value={this.state.wednesday}
+                                            id="wednesday"
+                                            type="checkbox"
+                                            className="form-check-input"
+                                        />
+                                        <label className="form-check-label" htmlFor="wednesday">Wednesday</label>
+                                        
+                                        <input
+                                            onChange={() => this.setState({thursday: !this.state.thursday})}
+                                            value={this.state.thursday}
+                                            id="thursday"
+                                            type="checkbox"
+                                            className="form-check-input"
+                                        />
+                                        <label className="form-check-label" htmlFor="thursday">Thursday</label>
+                                        
+                                        <input
+                                            onChange={() => this.setState({friday: !this.state.friday})}
+                                            value={this.state.friday}
+                                            id="friday"
+                                            type="checkbox"
+                                            className="form-check-input"
+                                        />
+                                        <label className="form-check-label" htmlFor="friday">Friday</label>
+                                    </div>
 
-                                <div className="form-label-group">
-                                    <input
-                                        onChange={this.onChange}
-                                        value={this.state.seatsRemaining}
-                                        id="seatsRemaining"
-                                        type="number"
-                                        className="form-control"
-                                    />
-                                    <label htmlFor="seatsRemaining">Seats Remaining</label>
-                                </div>
+                                    <div className="form-label-group">
+                                        <input
+                                            onChange={this.onChange}
+                                            value={this.state.seatsRemaining}
+                                            id="seatsRemaining"
+                                            type="number"
+                                            className="form-control"
+                                        />
+                                        <label htmlFor="seatsRemaining">Seats Remaining</label>
+                                    </div>
 
 
-                                <hr/>   
-                                <button className="btn btn-lg btn-primary btn-block text-uppercase" type="button" onClick={() => this.onClick()}>
-                                    Submit
-                                </button>
-                            </form>
+                                    <hr/>   
+                                    <button className="btn btn-lg btn-primary btn-block text-uppercase" type="button" onClick={() => this.onClick()}>
+                                        Submit
+                                    </button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
