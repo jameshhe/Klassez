9a86c6acf1609d060933ff60c65b10eb2c935fb6
Fidelia Nawar <fidelianawar@Fidelias-MacBook-Pro.local>
2020-12-02 19:25:59 -0600

    changing profilePage

--- frontend/src/components/profile/profilePage.jsx
+++ frontend/src/components/profile/profilePage.jsx
@@ -8,17 +8,19 @@ export class ProfilePage extends React.Component {
     profileRepository = new ProfileRepository()
 
     constructor(id, firstName, lastName, profilePic, biography, year, major, minor, concentration, classification, preferredHours){
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.profilePic = profilePic;
-        this.biography = biography;
-        this.year = year;
-        this.major = major;
-        this.minor = minor;
-        this.concentration = concentration;
-        this.classification = classification;
-        this.preferredHours = preferredHours;
+        this.state = {
+            id: 0,
+            firstName: "",
+            lastName: "",
+            profilePic: "",
+            biography: "",
+            year: "",
+            major: "",
+            minor: "",
+            concentration: "",
+            classification: "",
+            preferredHours: ""
+        };
     }
 
     // state = {
